e255a4fb11f9205a72e2c785c54aed1af89fd150
Abhishek <abhishek@Kabhi.local>
2025-07-18 08:49:29 +0530

    nav changes 17

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -57,7 +57,7 @@ export function Navbar() {
       )}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center min-h-[4rem] md:min-h-[5rem]">
+        <div className="flex justify-between items-center min-h-[5rem] md:min-h-[5rem]">
           {/* Logo and Brand Left */}
           <div className="flex items-center space-x-3 min-w-[280px] ml-0">
             <img
