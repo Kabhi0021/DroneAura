26f2c13debd87c5d10e99147291387e2da7b1609
Abhishek <abhishek@Kabhi.local>
2025-07-23 03:51:50 +0530

    staging set 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,16 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-
 export default defineConfig(({ mode }) => ({
   base: process.env.PREVIEW === "true"
-    ? "/DroneAura/preview/"
-    : "/DroneAura/",  
+    ? "/preview/"
+    : "/",
   server: {
-    host: "::",
+    host: "0.0.0.0",
     port: 8080,
   },
-  plugins: [
-    react(),
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
