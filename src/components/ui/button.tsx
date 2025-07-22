6fc2197a64527a264fe75f899737e262d1d0d5f2
Abhishek <abhishek@Kabhi.local>
2025-07-22 21:34:28 +0530

    button changes 6

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,11 +16,8 @@ const buttonVariants = cva(
       variant: {
         default: "",
         clicked: "",
-        //destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-
-        //secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        //destructive:"bg-destructive text-destructive-foreground hover:bg-destructive/90",
+        //secondary:"bg-secondary text-secondary-foreground hover:bg-secondary/80",
         //ghost: "hover:bg-accent hover:text-accent-foreground",
         //link: "text-primary underline-offset-4 hover:underline",
         hero: "[background:linear-gradient(90deg,#e32cea_2%,#f61dce_8%,#f35bbf_92%,#f16dac_98%)] text-white font-medium shadow-md hover:scale-100 active:scale-105 ", //Pink gradient
