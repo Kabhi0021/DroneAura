222378e4fae31316e89539a305c5c64f5973b0de
Abhishek <abhishek@Kabhi.local>
2025-07-23 01:24:56 +0530

    test

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,17 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/', // REQUIRED for custom domain!
+  base:
+    process.env.PREVIEW === 'true'
+      ? '/DroneAura/preview/'
+      : '/', // '/' for prod domain, '/DroneAura/preview/' for preview
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
