8847db08b59be74ce0226a1d6da3903bb03b7c44
Abhishek <abhishek@Kabhi.local>
2025-07-23 03:22:47 +0530

    staging set

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,19 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
+
 export default defineConfig(({ mode }) => ({
-  base:
-    process.env.PREVIEW === 'true'
-      ? '/DroneAura/preview/'
-      : '/', // '/' for prod domain, '/DroneAura/preview/' for preview
+  base: process.env.PREVIEW === "true"
+    ? "/DroneAura/preview/"
+    : "/DroneAura/",  
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
